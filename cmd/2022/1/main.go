4c207baeaafb4217d68ba8efed2d99b25f6dc908
Erik Lind <erik.lind@tiqqe.com>
2022-12-01 06:39:50 +0100

    Day 1 🍫

--- cmd/2022/1/main.go
+++ cmd/2022/1/main.go
@@ -2,18 +2,84 @@ package main
 
 import (
 	"aoc2022/internal/day"
-	"errors"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	day := day.New("Day 1", cmd, cmd2)
+	day := day.New("Day 1: Calorie Counting", cmd, cmd2)
 	day.Run()
 }
 
 func cmd(input string) (string, error) {
-	return "1", nil
+	inputArr := strings.Split(input, "\n")
+	calorieElfMap := make(map[int]int)
+
+	elf := 1
+	for _, calStr := range inputArr {
+		if calStr == "" {
+			elf++
+			continue
+		}
+
+		cal, err := strconv.Atoi(calStr)
+		if err != nil {
+			return "", err
+		}
+
+		calorieElfMap[elf] += cal
+	}
+
+	count := 0
+	for _, v := range calorieElfMap {
+		if count == 0 {
+			count = v
+		}
+
+		if v > count {
+			count = v
+		}
+	}
+
+	return fmt.Sprint(count), nil
 }
 
 func cmd2(input string) (string, error) {
-	return "", errors.New("NOT IMPLEMENTED")
+	inputArr := strings.Split(input, "\n")
+	calorieElfMap := make(map[int]int)
+
+	elf := 1
+	for _, calStr := range inputArr {
+		if calStr == "" {
+			elf++
+			continue
+		}
+
+		cal, err := strconv.Atoi(calStr)
+		if err != nil {
+			return "", err
+		}
+
+		calorieElfMap[elf] += cal
+	}
+
+	foods := make([]int, len(calorieElfMap))
+	for i := 0; i < len(calorieElfMap); i++ {
+		foods[i] = calorieElfMap[i+1]
+	}
+
+	sort.Slice(foods, func(i, j int) bool {
+		return foods[i] < foods[j]
+	})
+
+	top3 := foods[len(foods)-3:]
+
+	sum := 0
+	for _, v := range top3 {
+		sum += v
+	}
+
+	return fmt.Sprint(sum), nil
 }
