809f38cf5cdca4eac173c2441bde1d51b1c3e8dc
Erik Lind <erik.lind@tiqqe.com>
2022-12-06 07:18:26 +0100

    Day 6

--- cmd/2022/6/main.go
+++ cmd/2022/6/main.go
@@ -1,12 +1,49 @@
 package main
 
-import "aoc2022/internal/day"
+import (
+	"aoc2022/internal/day"
+	"bytes"
+	"fmt"
+)
 
 func main() {
-	day := day.New("Day 6", cmd)
+	day := day.New("Day 6: Tuning Trouble", cmd, cmd2)
 	day.Run()
 }
 
+func getIndex(input string, n int) int {
+	buf := bytes.NewBufferString(input)
+	sequence := make([]byte, 0, n)
+
+	i := 0
+	for {
+		i++
+		b := buf.Next(1)
+		if len(b) == 0 {
+			i = -1
+			break
+		}
+
+		if len(sequence) == n {
+			i--
+			break
+		}
+
+		if !bytes.Contains(sequence, b) {
+			sequence = append(sequence, b...)
+		} else {
+			s := bytes.IndexByte(sequence, b[0])
+			sequence = sequence[s+1:]
+			sequence = append(sequence, b...)
+		}
+	}
+	return i
+}
+
 func cmd(input string) (string, error) {
-	return input, nil
+	return fmt.Sprint(getIndex(input, 4)), nil
+}
+
+func cmd2(input string) (string, error) {
+	return fmt.Sprint(getIndex(input, 14)), nil
 }
